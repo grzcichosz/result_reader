8827b0602dbe8432413f5e6b16e034e69e052d63
Grzegorz <grzcichosz@gmail.com>
2021-11-01 19:10:15 +0100

    Initial commit Result reader

--- main.py
+++ main.py
@@ -6,33 +6,30 @@ config = {
     'password': 'example',
     'host': '127.0.0.1',
     'port': '13306',
-    'database': 'Patients',
+    'database': 'flaskweb',
     'raise_on_warnings': True
 }
 
 cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
 
-add_patient = ("INSERT INTO Patientsdata "
-               "(name, surname, birth, pesel, sex, sampleid)"
-               "VALUES (%s, %s, %s, %s, %s, %s)")
+add_result = ("INSERT INTO results "
+               "(sample_id, date, result)"
+               "VALUES (%s, %s, %s)")
 
 with open('Data.txt') as f:
     csr = csv.reader(f, dialect="excel-tab")
     firstLine = next(csr)
-    if firstLine == ['name', 'surname', 'birth', 'pesel', 'sex', 'sampleid']:
+    if firstLine == ['sample_id', 'date', 'result']:
         print("Wczytano")
     else:
         print("Nie wczytano")
     for line in csr:
         print(line)
-        cursor.execute(add_patient, line)
-
-
-#cursor.execute(add_employee, ('adam12', 'surname12'))
+        print(add_result, line)
+        cursor.execute(add_result, line)
 
 cnx.commit()
-
 cursor.close()
 cnx.close()
 
